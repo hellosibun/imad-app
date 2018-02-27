c52254ff5ae7e5a83c2eba1eadbcf24c42d1f2af
Soumya Ranjan Tripathy <34995065+hellosibun@users.noreply.github.com>
2018-02-27 20:55:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,10 +9,10 @@ var marginLeft = 0;
 function moveRight () {
     
     marginLeft=marginLeft+10;
-    img.style.marginLeft + "px";
+    img.style.marginLeft = marginLeft + "px";
     
 }
 img.onclick = function () {
 
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
 };
